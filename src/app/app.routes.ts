397753210cd8281499d8ec609b0a20da3e339c42
Nathandiv <k.n.hlungwani@gmail.com>
2024-08-28 14:41:33 +0200

    added angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+
+export const routes: Routes = [
+    {
+    path :'home',
+    component : HomeComponent
+},
+
+{
+    path :'about',
+    component : AboutComponent
+},
+
+
+{
+    path :'services',
+    component : ServicesComponent
+},
+
+];
